6e3f66d0563ab8093346b87a2c85819c2301adbd
Ruslan Yakovlev <68195302+Sylex84@users.noreply.github.com>
2022-07-26 01:24:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+class Calculator {
+	constructor(previousItemTextElement, currentItemTextElement) {
+		this.previousItemTextElement = previousItemTextElement
+		this.currentItemTextElement = currentItemTextElement
+		this.clear()
+	} 
+
+	clear() {
+		this.currentItem = ''
+		this.previousItem = ''
+		this.operation = undefined
+	}
+
+	delete() {
+		this.currentItem = this.currentItem.slice(0, -1)
+	}
+
+	appendNumber(number, count) {
+		if (number === '.' && this.currentItem.includes('.')) return
+		if (count <= 15) {
+			this.currentItem = this.currentItem + number
+		}
+	}
+
+	chooseOperation(operation) {
+		if (this.currentItem === '') return
+		if (this.previousItem !== '') {
+			this.compute()
+		}
+		count = 0
+		this.operation = operation
+		this.previousItem = this.currentItem
+		this.currentItem = ''
+	}
+
+	compute() {
+		let computation
+		const prev = parseFloat(this.previousItem)
+		const current = parseFloat(this.currentItem)
+		if (isNaN(prev) || isNaN(current)) return
+		switch (this.operation) {
+			case '+':
+				computation = prev + current
+				break
+			case '-':
+				computation = prev - current
+				break
+			case '*':
+				computation = prev * current
+				break
+			case '÷':
+				computation = prev / current
+				break
+			case '%':
+				computation = prev % current
+				break
+			default:
+				return
+		}
+		this.previousItem = ''
+		this.currentItem = computation.toString()
+		this.operation = undefined
+	}
+
+	getDisplayNumber(number) {
+		const stringNumber = number.toString()
+		const integerDigits = parseFloat(stringNumber.split('.')[0])
+		const decimalDigits = stringNumber.split('.')[1]
+		let integerDisplay
+		if (isNaN(integerDigits)) {
+			integerDisplay = ''
+		} else {
+			integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+		}
+		if (decimalDigits != null) {
+			return `${integerDisplay}.${decimalDigits}`
+		} else {
+			return integerDisplay
+		}
+	}
+
+	updateDisplay() {
+		this.currentItemTextElement.textContent = this.getDisplayNumber(this.currentItem)
+		if (this.operation != null) {
+			this.previousItemTextElement.textContent = 
+				`${this.getDisplayNumber(this.previousItem)} ${this.operation}`
+		} else {
+			this.previousItemTextElement.textContent = ''
+		}
+	}
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const previousItemTextElement = document.querySelector('[data-previous-item]')
+const currentItemTextElement = document.querySelector('[data-current-item]')
+
+const calculator = new Calculator(previousItemTextElement, currentItemTextElement) 
+let count = 0
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		count++
+		calculator.appendNumber(button.textContent, count)
+		calculator.updateDisplay()
+	})
+})
+
+operationButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.textContent)
+		calculator.updateDisplay()
+	})
+})
+
+equalsButton.addEventListener('click', button => {
+	calculator.compute()
+	calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+	calculator.clear()
+	calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+	calculator.delete()
+	calculator.updateDisplay()
+})
